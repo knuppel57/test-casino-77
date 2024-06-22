c10618bf34c88f9fcdf1c2868213aab7549f58a7
knuppel57 <98462112+knuppel57@users.noreply.github.com>
2024-06-22 13:27:45 +0200

    Update index.tsx

--- src/games/index.tsx
+++ src/games/index.tsx
@@ -27,7 +27,7 @@ export const GAMES: GameBundle[] = [
   {
     id: 'slots',
     meta: {
-      background: '#5465ff',
+      background: '#5A0361',
       name: 'Slots',
       image: '/games/slots.png',
       description: `
@@ -44,7 +44,7 @@ export const GAMES: GameBundle[] = [
          Ah, Flip, the quintessential quandary of chaos! Imagine, if you will, a coin twirling in the air, a simple yet scintillating gamble. Heads or Tails, the choice is yours! Double your money with a single flip or watch it vanish into the void. A game so pure, so primal, it pits your luck against the fates themselves. Each flip, a test of your nerve, your decision-making dancing on the edge of oblivion. Heads, you win; tails, you lose! The thrill, the agony, the ecstasy of chance! Ha ha ha!
       `,
       image: '/games/flip.png',
-      background: '#ffe694',
+      background: '#5A0361',
     },
     app: React.lazy(() => import('./Flip')),
   },
@@ -56,7 +56,7 @@ export const GAMES: GameBundle[] = [
       description: `
         Ah, HiLo, the dance of destiny and daring! Imagine, my dear player, a game where your wits and whims collide, a tantalizing test of whether the next card teeters higher or dives lower. Each guess, a step deeper into the abyss of fortune. Nail it right, again and again, and watch your spoils soar! But beware, know when to snatch your riches and run, lest the house has the last laugh! Ha ha ha!
       `,
-      background: '#ff4f4f',
+      background: '#5A0361',
     },
     props: { logo: '/logo.svg' },
     app: React.lazy(() => import('./HiLo')),
@@ -69,7 +69,7 @@ export const GAMES: GameBundle[] = [
         Ah, a game of hidden hazards and hidden treasures! Picture this, my mischievous mates: a board with money lurking beneath the squares. The more squares you reveal, the richer you become. But beware, for five fiendish mines are scattered about, waiting to blast your dreams to bits! Touch one, and poof—you're penniless! Yet, you can cash out at any time, if you've got the nerve to walk away. A tantalizing tango of risk and reward, where every reveal is a step closer to fortune—or folly. Dare to play, and let the chaos commence! Ha ha ha!
       `,
       image: '/games/mines.png',
-      background: '#8376ff',
+      background: '#5A0361',
     },
     app: React.lazy(() => import('./Mines')),
   },
@@ -81,14 +81,14 @@ export const GAMES: GameBundle[] = [
       description: `
         Roulette, my fiendish friends, is the grand gala of gambling brought to life in digital decadence! Spin the wheel, place your bets, and let the little ball of fate dance across the numbers. Will it land on your lucky spot or dash your dreams into dust? The rules are as simple as a joker's jibe, yet the rewards—oh, the rewards—are as grand as Gotham's tallest towers. A timeless tempest of chance and fortune, where every spin teeters on the edge of triumph or tragedy. Come, take a whirl, and let the wheel decide your destiny! Ha ha ha!
       `,
-      background: '#1de87e',
+      background: '#5A0361',
     },
     app: React.lazy(() => import('./Roulette')),
   },
   {
     id: 'plinko',
     meta: {
-      background: '#7272ff',
+      background: '#5A0361',
       image: '/games/plinko.png',
       name: 'Plinko',
       description: `
@@ -101,7 +101,7 @@ export const GAMES: GameBundle[] = [
   {
     id: 'crash',
     meta: {
-      background: '#de95e8',
+      background: '#5A0361',
       image: '/games/crash.png',
       name: 'Crash',
       description: `
