8a9dc29fd8972b5eddef465544ce8ef598c97973
knuppel57 <98462112+knuppel57@users.noreply.github.com>
2024-06-22 14:46:39 +0200

    Update Dashboard.tsx

--- src/sections/Dashboard/Dashboard.tsx
+++ src/sections/Dashboard/Dashboard.tsx
@@ -46,7 +46,7 @@ export default function Dashboard() {
   return (
     <>
       <WelcomeBanner />
-      <h2 style={{ textAlign: 'center' }}>Why so Serious?</h2>
+      <h2 style={{ textAlign: 'center' }}>Joker Casino</h2>
       <GameGrid />
     </>
   )
