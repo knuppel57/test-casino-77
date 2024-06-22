3096ea08302369f9b366b7c2575848145ff24bac
knuppel57 <98462112+knuppel57@users.noreply.github.com>
2024-06-22 17:48:31 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -33,10 +33,10 @@ const lp = (tokenMint: PublicKey | string, poolAuthority?: PublicKey | string): 
  * For private pools, add the creator of the Liquidity Pool as a second argument
  */
 export const POOLS = [
-  // Fake token:
-  lp(FAKE_TOKEN_MINT),
   // SOL:
   lp('So11111111111111111111111111111111111111112'),
+  // Fake token:
+  lp(FAKE_TOKEN_MINT),
   // USDC:
   lp('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
   // Wormhole:
