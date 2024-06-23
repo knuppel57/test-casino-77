e8338557335267d935c4ea247030cdf8140a017c
knuppel57 <98462112+knuppel57@users.noreply.github.com>
2024-06-23 11:18:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -76,6 +76,7 @@ export default function App() {
           <Route path="/:gameId" element={<Game />} />
         </Routes>
         <h2 style={{ textAlign: 'center' }}>Why so serious?</h2>
+          <h1 style={{ textAlign: 'center' }}>Why so serious 2?</h1>
         <RecentPlays />
       </MainWrapper>
     </>
